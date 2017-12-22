575d42eba78824ceffd03ed6dd29f68cfad81fa7
eduardocollm <coll.eduardo@gmail.com>
2017-12-22 16:38:50 -0600

    initial commit

---
+++ app/src/main/java/com/collesoft/android/firstapp/DisplayMessageActivity.java
@@ -0,0 +1,22 @@
+package com.collesoft.android.firstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Get intent that started this activity
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
